a44b31536bf8429c9bacefee9e74e0950c2e90b5
ovelychko <Alex.Velichko@gmail.com>
2021-10-04 23:46:21 +0300

    Added image file to the resources

--- src/main/java/com/ovelychko/webhookbotspring/TelegramBot.java
+++ src/main/java/com/ovelychko/webhookbotspring/TelegramBot.java
@@ -6,11 +6,14 @@ import org.springframework.context.annotation.Configuration;
 import org.telegram.telegrambots.bots.TelegramWebhookBot;
 import org.telegram.telegrambots.meta.api.methods.BotApiMethod;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.methods.send.SendPhoto;
+import org.telegram.telegrambots.meta.api.objects.InputFile;
 import org.telegram.telegrambots.meta.api.objects.Update;
 
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.Reader;
+import java.net.URL;
 import java.util.Properties;
 import java.util.Random;
 import java.util.StringJoiner;
@@ -19,7 +22,8 @@ import java.util.StringJoiner;
 public class TelegramBot extends TelegramWebhookBot {
     private static final Logger logger = LoggerFactory.getLogger(TelegramBot.class);
     private final TelegramBotConfig telegramBotConfig;
-    private final String CARD_FILE = "tarocards.properties";
+    private final String CARD_FILE = "tarotcards.properties";
+    private final String IMAGE_FILE = "tarotimages/testCard.jpg";
     private final int CARDS_COUNT = 78;
     private final String ENDL = "\n";
 
@@ -70,12 +74,26 @@ public class TelegramBot extends TelegramWebhookBot {
                 int num = random.nextInt(CARDS_COUNT + 1) + 1;
                 String cardOfTheDay = property.getProperty("card_" + num);
                 joiner.add(cardOfTheDay);
+                joiner.add("debug value: " + num);
             } catch (Exception ex) {
                 joiner.add("Exception: " + ex.getMessage());
                 logger.info("Exception: {}", ex.getMessage());
             }
             sendMessage.setText(joiner.toString());
             logger.info(joiner.toString());
+
+            try {
+                ClassLoader classLoader = getClass().getClassLoader();
+                URL url = classLoader.getResource(IMAGE_FILE);
+
+                SendPhoto message = new SendPhoto();
+                message.setChatId(update.getMessage().getChatId().toString());
+                message.setCaption("Caption Photo");
+                message.setPhoto(new InputFile(url.openStream(), "File Name Photo"));
+                this.execute(message);
+            } catch (Exception ex) {
+                logger.info(ex.getMessage());
+            }
             return sendMessage;
         }
         return null;
